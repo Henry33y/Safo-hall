0ed97795088a7c7bc50879126d7f61b34ee710c2
Henry <henryasante336@gmail.com>
2024-01-18 14:13:19 +0000

    Created mobile nav

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const hamburgerBtn = document.querySelector('.hamburger-btn');
+const mobileNav = document.querySelector('.mobile-nav-container');
 
 hamburgerBtn.addEventListener('click',()=>{
     hamburgerBtn.classList.toggle('clicked');
+    if(hamburgerBtn.classList.contains('clicked')){
+        mobileNav.style.display = 'flex';
+    }
+    else{
+        mobileNav.style.display = 'none';
 
+    }
 })
